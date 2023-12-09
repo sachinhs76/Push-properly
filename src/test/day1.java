dc28f27b307b710699913fcbd922075511c87461
sachinhs76 <sachinhs76@gmail.com>
2023-12-09 12:34:37 -0500

    testng commit

---
+++ src/test/day1.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day1 
+{
+	
+ @Test
+ public void demo()
+ {
+		System.out.println("Demo test");
+ }
+
+	 
+
+}
---
+++ src/test/day2.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day2 {
+ @Test
+ public void mblogin()
+ {
+		System.out.println("mobile login");
+ }
+ @Test
+ public void mblogout()
+ {
+	 System.out.println("mobile logout");
+ }
+	 
+
+}
